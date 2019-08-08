dac79c62851bbf0796bd69da714f628d7b6fb261
sudh-17 <395338095@qq.com>
2019-08-08 23:34:50 +0800

    todo mvc

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,26 +1,34 @@
 <template>
-    <div>
-        <ul>
+  <div>
+    <!-- <ul>
             <li v-for="item in list" :key="item.id" >
                 <slot name="id" v-bind:id="item.id">{{item.id}}</slot>
-                <slot @click="del('1111')" name="title" v-bind:title="item.name">{{item.name}}</slot>
+                <slot name="title" v-bind:title="item.name">{{item.name}}</slot>
             </li>
-        </ul>
-    </div>
+    </ul>-->
+    <ul class="todo-list">
+      <li v-for="item in list" :key="item.id" data-id="1546478446428" class="completed">
+        <div class="view">
+          <input class="toggle" type="checkbox" />
+          <label>{{ item.title}}</label>
+          <button class="destroy" @click="onDel(item.id)"></button>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name:  'list',
-    props: {
-        list: Array
-    },
-    methods: {
-        del (id) {
-            console.log('id', id)
-            this.$emit('del', id)
-        }
+  name: "list",
+  props: {
+    list: Array
+  },
+  methods: {
+    onDel (id) {
+      this.$emit("onDel", id);
     }
-}
+  }
+};
 </script>
 
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,52 +1,99 @@
 <template>
-    <section class="todoapp">
-        <header class="header">
-            <h1>todos</h1>
-            <input class="new-todo" placeholder="What needs to be done?" autofocus="">
-        </header>
-        <section class="main" style="display: block;">
-            <input id="toggle-all" class="toggle-all" type="checkbox">
-            <label for="toggle-all">Mark all as complete</label>
-            <ul class="todo-list">
-                <li data-id="1546478446428" class="completed">
-                    <div class="view">
-                        <input class="toggle" type="checkbox">
-                        <label>f</label>
-                        <button class="destroy"></button></div>
-                </li>
-                <li data-id="1546478447470" class="completed">
-                    <div class="view">
-                        <input class="toggle" type="checkbox">
-                        <label>dw</label>
-                        <button class="destroy"></button>
-                    </div>
-                </li>
-            </ul>
-        </section>
-        <footer class="footer" style="display: block;">
-            <span class="todo-count"><strong>2</strong> items left</span>
-            <ul class="filters">
-                <li>
-                    <a href="#/" class="selected">All</a>
-                </li>
-                <li>
-                    <a href="#/active">Active</a>
-                </li>
-                <li>
-                    <a href="#/completed">Completed</a>
-                </li>
-            </ul>
-            <button class="clear-completed" style="display: block;">Clear completed</button>
-        </footer>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        @keyup.enter="addItem"
+        class="new-todo"
+        placeholder="What needs to be completed?"
+        autofocus
+      />
+    </header>
+
+    <section class="main" style="display: block;">
+      <template v-if="list.length > 0">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+      </template>
+      <List :list="list" @onDel="delItem" />
     </section>
+
+    <footer v-if="list.length > 0" class="footer" style="display: block;">
+      <span class="todo-count">
+        <strong>2</strong> items left
+      </span>
+      <ul class="filters">
+        <li>
+          <a href="#/" class="selected">All</a>
+        </li>
+        <li>
+          <a href="#/active">Active</a>
+        </li>
+        <li>
+          <a href="#/completed">Completed</a>
+        </li>
+      </ul>
+      <button class="clear-completed" style="display: block;">Clear completed</button>
+    </footer>
+  </section>
 </template>
 
 <script>
-import './base.css'
-import './common.css'
+import "./base.css";
+import "./common.css";
+import List from "../components/List.vue";
+
+let list = [
+  {
+    id: "1",
+    completed: false,
+    title: "ff1"
+  },
+  {
+    id: "2",
+    completed: false,
+    title: "ff2"
+  },
+  {
+    id: "3",
+    completed: false,
+    title: "ff3"
+  },
+  {
+    id: "4",
+    completed: false,
+    title: "ff4"
+  }
+];
 export default {
-    
-}
+  name: "home",
+  data() {
+    return {
+      list
+    };
+  },
+  components: {
+    List
+  },
+  methods: {
+    addItem(e) {
+      let val = e.target.value;
+      if (!val || val.trim() === "") {
+        return;
+      }
+      this.list.push({
+        id: new Date().getTime().toString(),
+        title: val,
+        completed: false
+      });
+      e.target.value = "";
+    },
+    delItem(id) {
+      let index = this.list.findIndex(item => item.id === id);
+      this.list.splice(index, 1);
+    }
+  }
+};
 </script>
 
 <style lang="less">
